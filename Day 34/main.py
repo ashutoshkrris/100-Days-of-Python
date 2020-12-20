f8300fd25cb9383dfef4245ce496a8d63bfd49e4
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-12-20 12:03:03 +0530

    Add Day 34

---
+++ Day 34/main.py
@@ -0,0 +1,13 @@
+text: str
+age: int
+is_human: bool
+
+
+text = "hi"
+
+
+def fun(text: str) -> str:
+    print(f"{text} is a string")
+
+
+fun("hello")
---
+++ Day 34/quizzler-app/data.py
@@ -0,0 +1,10 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+question_data = response.json()["results"]
+
