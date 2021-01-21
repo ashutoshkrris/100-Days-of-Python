0eb85928044ab83ac5af61c6c9f71cc49035b8e3
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-01-21 20:47:38 +0530

    Add Day 56

---
+++ Day 56/name-card/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    data = {
+        "Status": 200,
+        "Message": "Up and Running"
+    }
+    return data
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
